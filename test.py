3c442ecc9882433dcd46c874e790ecf1c7266748
LI-DU <42836478+LI-DU@users.noreply.github.com>
2018-12-16 16:51:25 +0800

    Add files via upload
    test file

--- test.py
+++ test.py
@@ -1,25 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+def insert_sort(old_list):
+    n = len(old_list)
+    for i in range(1,n):
+        temp = old_list[i]
+        j = i
+        while j > 0 and temp < old_list[j-1]:
+            old_list[j] = old_list[j-1]
+            j = j-1
+            old_list[j] = temp
+        return old_list
+        
+old_list = [1,9,5,0,-2,38,20,-9,6]
+insert_sort(old_list)
 
-
-class Config:   #　定义配置类　封装所有的配置，方便对配置统一的管理
-    # 定义和配置同名的类属性
-    DEBUG = True #　设置调试模式
-    #　配置数据库连接地址
-    SQLALCHEMY_TRACK_MODIFICATIONS = "mysql://root:mysql@127.0.0.1:3306/info19"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 设置追踪数据库变化
-
-# 从对象加载配置信息
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-
-@app.route('/')
-def index():
-    return "index"
-
-
-if __name__ == '__main__':
-    app.run()
